5cf10f0755c533c533aa4b9718462ef1bab3e6f7
Rich Kingsford <richkingsford@gmail.com>
2026-06-12 10:32:54 -0600

    Add videos page and analytics tracking

---
+++ src/main/webapp/js/google-analytics.js
@@ -0,0 +1,19 @@
+(function() {
+  var measurementId = 'G-8R72Y3748X';
+  var trackedHosts = ['www.chemvantage.org', 'chemvantage.org'];
+
+  if (trackedHosts.indexOf(window.location.hostname) === -1) return;
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function() {
+    window.dataLayer.push(arguments);
+  };
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(measurementId);
+  document.head.appendChild(script);
+
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId);
+})();
